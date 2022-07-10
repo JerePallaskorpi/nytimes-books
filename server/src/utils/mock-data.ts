3a2b0bb573eec416d27c1e125bdfa74e473d5a54
Jere Pallaskorpi <jere.pallaskorpi@gmail.com>
2022-07-10 16:26:28 +0300

    Use example data to bypass API limit

---
+++ server/src/utils/mock-data.ts
@@ -0,0 +1,356 @@
+export const exampleCategoriesResponse = [
+    {
+        "list_name_encoded": "combined-print-and-e-book-fiction",
+        "display_name": "Combined Print & E-Book Fiction"
+    },
+    {
+        "list_name_encoded": "combined-print-and-e-book-nonfiction",
+        "display_name": "Combined Print & E-Book Nonfiction"
+    },
+    {
+        "list_name_encoded": "hardcover-fiction",
+        "display_name": "Hardcover Fiction"
+    },
+    {
+        "list_name_encoded": "hardcover-nonfiction",
+        "display_name": "Hardcover Nonfiction"
+    },
+    {
+        "list_name_encoded": "trade-fiction-paperback",
+        "display_name": "Paperback Trade Fiction"
+    },
+    {
+        "list_name_encoded": "mass-market-paperback",
+        "display_name": "Paperback Mass-Market Fiction"
+    },
+    {
+        "list_name_encoded": "paperback-nonfiction",
+        "display_name": "Paperback Nonfiction"
+    },
+    {
+        "list_name_encoded": "e-book-fiction",
+        "display_name": "E-Book Fiction"
+    },
+    {
+        "list_name_encoded": "e-book-nonfiction",
+        "display_name": "E-Book Nonfiction"
+    },
+    {
+        "list_name_encoded": "hardcover-advice",
+        "display_name": "Hardcover Advice & Misc."
+    },
+    {
+        "list_name_encoded": "paperback-advice",
+        "display_name": "Paperback Advice & Misc."
+    },
+    {
+        "list_name_encoded": "advice-how-to-and-miscellaneous",
+        "display_name": "Advice, How-To & Miscellaneous"
+    },
+    {
+        "list_name_encoded": "hardcover-graphic-books",
+        "display_name": "Hardcover Graphic Books"
+    },
+    {
+        "list_name_encoded": "paperback-graphic-books",
+        "display_name": "Paperback Graphic Books"
+    },
+    {
+        "list_name_encoded": "manga",
+        "display_name": "Manga"
+    },
+    {
+        "list_name_encoded": "combined-print-fiction",
+        "display_name": "Combined Hardcover & Paperback Fiction"
+    },
+    {
+        "list_name_encoded": "combined-print-nonfiction",
+        "display_name": "Combined Hardcover & Paperback Nonfiction"
+    },
+    {
+        "list_name_encoded": "chapter-books",
+        "display_name": "Children’s Chapter Books"
+    },
+    {
+        "list_name_encoded": "childrens-middle-grade",
+        "display_name": "Children’s Middle Grade"
+    },
+    {
+        "list_name_encoded": "childrens-middle-grade-e-book",
+        "display_name": "Children’s Middle Grade E-Book"
+    },
+    {
+        "list_name_encoded": "childrens-middle-grade-hardcover",
+        "display_name": "Children’s Middle Grade Hardcover"
+    },
+    {
+        "list_name_encoded": "childrens-middle-grade-paperback",
+        "display_name": "Children’s Middle Grade Paperback"
+    },
+    {
+        "list_name_encoded": "paperback-books",
+        "display_name": "Children’s Paperback Books"
+    },
+    {
+        "list_name_encoded": "picture-books",
+        "display_name": "Children’s Picture Books"
+    },
+    {
+        "list_name_encoded": "series-books",
+        "display_name": "Children’s Series"
+    },
+    {
+        "list_name_encoded": "young-adult",
+        "display_name": "Young Adult"
+    },
+    {
+        "list_name_encoded": "young-adult-e-book",
+        "display_name": "Young Adult E-Book"
+    },
+    {
+        "list_name_encoded": "young-adult-hardcover",
+        "display_name": "Young Adult Hardcover"
+    },
+    {
+        "list_name_encoded": "young-adult-paperback",
+        "display_name": "Young Adult Paperback"
+    },
+    {
+        "list_name_encoded": "animals",
+        "display_name": "Animals"
+    },
+    {
+        "list_name_encoded": "audio-fiction",
+        "display_name": "Audio Fiction"
+    },
+    {
+        "list_name_encoded": "audio-nonfiction",
+        "display_name": "Audio Nonfiction"
+    },
+    {
+        "list_name_encoded": "business-books",
+        "display_name": "Business"
+    },
+    {
+        "list_name_encoded": "celebrities",
+        "display_name": "Celebrities"
+    },
+    {
+        "list_name_encoded": "crime-and-punishment",
+        "display_name": "Crime and Punishment"
+    },
+    {
+        "list_name_encoded": "culture",
+        "display_name": "Culture"
+    },
+    {
+        "list_name_encoded": "education",
+        "display_name": "Education"
+    },
+    {
+        "list_name_encoded": "espionage",
+        "display_name": "Espionage"
+    },
+    {
+        "list_name_encoded": "expeditions-disasters-and-adventures",
+        "display_name": "Expeditions"
+    },
+    {
+        "list_name_encoded": "fashion-manners-and-customs",
+        "display_name": "Fashion, Manners and Customs"
+    },
+    {
+        "list_name_encoded": "food-and-fitness",
+        "display_name": "Food and Diet"
+    },
+    {
+        "list_name_encoded": "games-and-activities",
+        "display_name": "Games and Activities"
+    },
+    {
+        "list_name_encoded": "graphic-books-and-manga",
+        "display_name": "Graphic Books and Manga"
+    },
+    {
+        "list_name_encoded": "hardcover-business-books",
+        "display_name": "Hardcover Business Books"
+    },
+    {
+        "list_name_encoded": "health",
+        "display_name": "Health"
+    },
+    {
+        "list_name_encoded": "humor",
+        "display_name": "Humor"
+    },
+    {
+        "list_name_encoded": "indigenous-americans",
+        "display_name": "Indigenous Americans"
+    },
+    {
+        "list_name_encoded": "relationships",
+        "display_name": "Love and Relationships"
+    },
+    {
+        "list_name_encoded": "mass-market-monthly",
+        "display_name": "Mass Market"
+    },
+    {
+        "list_name_encoded": "middle-grade-paperback-monthly",
+        "display_name": "Middle Grade Paperback"
+    },
+    {
+        "list_name_encoded": "paperback-business-books",
+        "display_name": "Paperback Business Books"
+    },
+    {
+        "list_name_encoded": "family",
+        "display_name": "Parenthood and Family"
+    },
+    {
+        "list_name_encoded": "hardcover-political-books",
+        "display_name": "Politics and American History"
+    },
+    {
+        "list_name_encoded": "race-and-civil-rights",
+        "display_name": "Race and Civil Rights"
+    },
+    {
+        "list_name_encoded": "religion-spirituality-and-faith",
+        "display_name": "Religion, Spirituality and Faith"
+    },
+    {
+        "list_name_encoded": "science",
+        "display_name": "Science"
+    },
+    {
+        "list_name_encoded": "sports",
+        "display_name": "Sports and Fitness"
+    },
+    {
+        "list_name_encoded": "travel",
+        "display_name": "Travel"
+    },
+    {
+        "list_name_encoded": "young-adult-paperback-monthly",
+        "display_name": "Young Adult Paperback"
+    }
+];
+
+export const exampleHardcoverFictionResponse = [
+    {
+        "title": "THE HOTEL NANTUCKET",
+        "author": "Elin Hilderbrand",
+        "primary_isbn10": "0316258679",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780316258678.jpg",
+        "rank": 1,
+        "reviews": [
+            {
+                "url": "https://www.nytimes.com/2022/05/26/books/review/the-hotel-nantucket-elin-hilderbrand.html",
+                "byline": "By Michelle Ruiz",
+                "summary": "Elin Hilderbrand and Jennifer Weiner deliver their annual installments of salty air, summer love and personal entanglements."
+            }
+        ]
+    },
+    {
+        "title": "SPARRING PARTNERS",
+        "author": "John Grisham",
+        "primary_isbn10": "0385549326",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780385549325.jpg",
+        "rank": 2,
+        "reviews": []
+    },
+    {
+        "title": "SUSPECTS",
+        "author": "Danielle Steel",
+        "primary_isbn10": "1984821679",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9781984821676.jpg",
+        "rank": 3,
+        "reviews": []
+    },
+    {
+        "title": "THE HOUSE ACROSS THE LAKE",
+        "author": "Riley Sager",
+        "primary_isbn10": "0593183193",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780593183199.jpg",
+        "rank": 4,
+        "reviews": []
+    },
+    {
+        "title": "ESCAPE",
+        "author": "James Patterson and David Ellis",
+        "primary_isbn10": "0316499447",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780316499446.jpg",
+        "rank": 5,
+        "reviews": []
+    },
+    {
+        "title": "THE MEASURE",
+        "author": "Nikki Erlick",
+        "primary_isbn10": "0063204207",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780063204201.jpg",
+        "rank": 6,
+        "reviews": [
+            {
+                "url": "https://www.nytimes.com/2022/06/28/books/review/the-measure-nikki-erlick.html",
+                "byline": "By Leni Zumas",
+                "summary": "In Nikki Erlick’s debut novel, “The Measure,” all adults can find out how much time they have left."
+            }
+        ]
+    },
+    {
+        "title": "THE MIDNIGHT LIBRARY",
+        "author": "Matt Haig",
+        "primary_isbn10": "0525559477",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780525559474.jpg",
+        "rank": 7,
+        "reviews": [
+            {
+                "url": "https://www.nytimes.com/2020/09/29/books/review/matt-haig-the-midnight-library.html",
+                "byline": "Karen Joy Fowler",
+                "summary": "Matt Haig provides a fresh literary twist on the “Sliding Doors” phenomenon."
+            },
+            {
+                "url": "https://www.nytimes.com/2020/09/29/books/review/the-midnight-library-by-matt-haig-an-excerpt.html",
+                "byline": "",
+                "summary": "An excerpt from “The Midnight Library,” by Matt Haig"
+            }
+        ]
+    },
+    {
+        "title": "LESSONS IN CHEMISTRY",
+        "author": "Bonnie Garmus",
+        "primary_isbn10": "038554734X",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780385547345.jpg",
+        "rank": 8,
+        "reviews": [
+            {
+                "url": "https://www.nytimes.com/2022/04/04/books/lessons-in-chemistry-bonnie-garmus.html",
+                "byline": "By Elisabeth Egan",
+                "summary": "In Bonnie Garmus’s debut novel, “Lessons in Chemistry,” a woman who has been banished to the home front turns it into a staging ground for a revolution."
+            }
+        ]
+    },
+    {
+        "title": "THE LAST THING HE TOLD ME",
+        "author": "Laura Dave",
+        "primary_isbn10": "1501171348",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9781501171345.jpg",
+        "rank": 9,
+        "reviews": []
+    },
+    {
+        "title": "HORSE",
+        "author": "Geraldine Brooks",
+        "primary_isbn10": "0399562966",
+        "book_image": "https://storage.googleapis.com/du-prd/books/images/9780399562969.jpg",
+        "rank": 10,
+        "reviews": [
+            {
+                "url": "https://www.nytimes.com/2022/06/07/books/review-horse-geraldine-brooks.html",
+                "byline": "By Alexandra Jacobs",
+                "summary": "Brooks’s latest novel focuses on two young Black men, and shuttles between the present day and the 19th-century world of horse racing."
+            }
+        ]
+    }
+];
